db68e2aaaacab1b38b15043422f9b84ef2950e00
Logan Buesching <lbuesching@salesforce.com>
2023-01-09 15:30:02 -0500

    Add NOW()

---
+++ src/Sage.Engine/Runtime/Functions/DateTime.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2022, salesforce.com, inc.
+// All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/Apache-2.0
+
+namespace Sage.Engine.Runtime
+{
+    public partial class RuntimeContext
+    {
+        /// <summary>
+        /// Returns the current system (server) date and time.
+        /// Now() is in Central Standard Time (CST) without daylight saving time.
+        /// </summary>
+        /// <param name="useSendTimeStarted">Determines whether to preserve the email sent time for post-send resolution of Now(). A value of true preserves the email sent time.</param>
+        public DateTimeOffset NOW(object? useSendTimeStarted = null)
+        {
+            if (useSendTimeStarted != null)
+            {
+                throw new NotImplementedException("useSendTimeStarted is not implemented");
+            }
+
+            // TODO: Use CST without daylight savings
+            return DateTimeOffset.Now;
+        }
+    }
+}
