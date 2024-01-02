0109c396014f367d5e674caeee0f038248de6290
lbuesching <85355760+lbuesching@users.noreply.github.com>
2024-01-02 13:30:47 -0500

    Create output directory if missing (#23)
    This fixes issue #18 where TREATASCONTENT will fail if the output directory is missing.

---
+++ src/Sage.Engine.Tests/Functions/ContentTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2022, salesforce.com, inc.
+// All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/Apache-2.0
+
+using Sage.Engine.Runtime;
+
+namespace Sage.Engine.Tests.Functions
+{
+    [TestFixture]
+    internal class ContentTests : FunctionTestBase
+    {
+        [Test]
+        [TestCase("%%=ADD(1,2)=%%", "3")]
+        public void TestTreatAsContentNoOutputDirectory(string input, string expected)
+        {
+            var options = TestCompilationOptions();
+            options.OutputDirectory = new DirectoryInfo(Path.Combine(options.OutputDirectory.FullName, "doesnotexist"));
+            var context = new RuntimeContext(_serviceProvider, options);
+
+            string results = context.TREATASCONTENT(input);
+            Assert.That(results, Is.EqualTo(expected));
+        }
+    }
+}
