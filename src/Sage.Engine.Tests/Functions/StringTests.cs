a220d335bae33075baa627b584f3fca3e1917fbc
Logan Buesching <lbuesching@salesforce.com>
2024-01-02 14:09:14 -0500

    Fix nullability with nunit

--- src/Sage.Engine.Tests/Functions/StringTests.cs
+++ src/Sage.Engine.Tests/Functions/StringTests.cs
@@ -12,7 +12,7 @@ namespace Sage.Engine.Tests.Functions
         [TestCase("Hello World", "Hello ", null, "World")]
         [TestCase("Hello 1World", "Hello ", 1, "World")]
         [TestCase("1World", null, 1, "World")]
-        public void TestConcat(string expected, params object[] arguments)
+        public void TestConcat(string expected, params object?[] arguments)
         {
             string actual = this._runtimeContext.CONCAT(arguments);
             Assert.That(actual, Is.EqualTo(expected));
@@ -25,7 +25,7 @@ namespace Sage.Engine.Tests.Functions
         [TestCase(null, "o", 0)]
         [TestCase(null, null, 0)]
         [TestCase("Hello", null, 0)]
-        public void TestIndexOf(string subject, string search, int expected)
+        public void TestIndexOf(string? subject, string? search, int expected)
         {
             int actual = this._runtimeContext.INDEXOF(subject, search);
             Assert.That(actual, Is.EqualTo(expected));
